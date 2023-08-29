862a700bafc00135e1697dc29bb88d0080623336
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-08-29 14:43:09 +0900

    소스정리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// import logo from './logo.svg';
-import { useEffect, useState } from 'react';
-
 import 'css/App.css';
 import Home from 'pages/Home';
 import {
@@ -13,23 +10,17 @@ import ScrollTop from 'include/ScrollTop';
 import Header from 'include/Header';
 import Wrapper from 'include/Wrapper';
 import Dimension from 'pages/Dimension';
-// import Footer from 'include/Footer';
-
 
 function App() {
   return (
-    //  basename={process.env.PUBLIC_URL}
-    //  
-    <BrowserRouter basename={'/react_sandion'}>
+    <BrowserRouter basename='react_sandion'>
       <Wrapper>
         <ScrollTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="/dimension" element={<Dimension />} />
-          {/* <Route path="/test" element={<Test />} /> */}
         </Routes>
-        {/* <Footer /> */}
       </Wrapper>
     </BrowserRouter>
   );
