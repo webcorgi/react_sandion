9b29106f033a005bc467a11559a4542b48f88bc4
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-08-29 17:14:01 +0900

    최종

--- src/pages/Dimension.js
+++ src/pages/Dimension.js
@@ -9,27 +9,27 @@ import img3_on from 'img/dimension3_on.png'
 function Dimension() {
     return (
         <div className="dimension">
-            <div className="box">
+            <a className="box" href="https://www.sandbox.game/en/experiences/AfterOcean%20:%20Color%20Blast/a4fc68ad-2d3f-4e77-b82b-7bfab901a743/page/" target="_blank">
                 <div className="num">01</div>
                 <div className="img">
                     <img src={img1_off} alt="img" />
                     <img src={img1_on} alt="img" />
                 </div>
-            </div>
-            <div className="box">
+            </a>
+            <a className="box" href="https://www.sandbox.game/en/experiences/AfterOcean%20:%20Adventure%20GREEN/c85994b7-1103-44f6-ab9e-ff1be9273ddf/page/" target="_blank">
                 <div className="num">02</div>
                 <div className="img">
                     <img src={img2_off} alt="img" />
                     <img src={img2_on} alt="img" />
                 </div>
-            </div>
-            <div className="box">
+            </a>
+            <a className="box" href="https://www.sandbox.game/en/experiences/AfterOcean%20:%20Canisty%20Run!/dd4599ad-88f8-4192-84eb-7b5d9d8c38a7/page/" target="_blank">
                 <div className="num">03</div>
                 <div className="img">
                     <img src={img3_off} alt="img" />
                     <img src={img3_on} alt="img" />
                 </div>
-            </div>
+            </a>
         </div>
     );
 }
