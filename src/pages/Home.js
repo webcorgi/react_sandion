2f470f1060d0e160974e3665ed8f04f81bf8efbe
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-10-05 16:49:22 +0900

    end 비디오 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import videoFront from 'video/SANDION_Main_front.mp4'
 import videoTurn from 'video/SANDION_Main_turn.mp4'
 import videoBack from 'video/SANDION_Main_back.mp4'
+import videoEnd from 'video/SANDION_Main_end.mp4'
 import mainLogo from 'img/main_logo.png'
 import { useState,useRef  } from "react";
 
@@ -10,39 +11,52 @@ function Home() {
     const frontRef = useRef(null);
     const turnRef = useRef(null);
     const backRef = useRef(null);
+    const endRef = useRef(null);
 
     const handleButtonOver = () => {
             frontRef.current.pause();
             turnRef.current.play();
             frontRef.current.style.zIndex=0;
+            endRef.current.style.zIndex=0;
+            endRef.current.style.display='block';
             turnRef.current.style.zIndex=1;
     }
 
     const handleButtonLeave = () => {
-        frontRef.current.style.zIndex=1;
-        turnRef.current.style.zIndex=0;
-        backRef.current.style.zIndex=0;
+        endRef.current.style.zIndex = 2;
+        frontRef.current.style.zIndex = 1;
+        turnRef.current.style.zIndex = 0;
+        backRef.current.style.zIndex = 0;
 
-        frontRef.current.setAttribute('loop', true);
-        frontRef.current.setAttribute('autoplay', true);
-        frontRef.current.play();
+        endRef.current.play();
 
-        turnRef.current.style.zIndex=0;
+        turnRef.current.style.zIndex = 0;
         turnRef.current.pause();
         turnRef.current.currentTime = 0;
 
-        backRef.current.setAttribute('loop', false);
-        backRef.current.setAttribute('autoplay', false);
-        backRef.current.pause();
+        backRef.current.setAttribute('loop', true);
+        backRef.current.setAttribute('autoplay', true);
+        backRef.current.play();
     }
 
     const handleTurnVideoEnd = () => {
         turnRef.current.style.zIndex=0;
-
         backRef.current.style.zIndex=2;
         backRef.current.setAttribute('loop', true);
         backRef.current.setAttribute('autoplay', true);
         backRef.current.play();
+    }
+
+    const handleEndVideoEnded = () => {
+        endRef.current.style.zIndex = 0;
+        backRef.current.style.zIndex = 0;
+        turnRef.current.style.zIndex = 0;
+        frontRef.current.style.zIndex = 2;
+
+        endRef.current.pause();
+        backRef.current.pause();
+        turnRef.current.pause();
+        frontRef.current.play();
     }
 
     const FrontComponent = () => {
@@ -69,6 +83,14 @@ function Home() {
             </video>
         )
     }
+    const EndComponent = () => {
+        return (
+            <video muted className="video__main end" ref={endRef}  onEnded={handleEndVideoEnded}>
+                <source src={videoEnd} type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+        )
+    }
 
 
     return (
@@ -82,6 +104,7 @@ function Home() {
             <FrontComponent />
             <TurnComponent />
             <BackComponent />
+            <EndComponent />
         </main>
     );
 }
